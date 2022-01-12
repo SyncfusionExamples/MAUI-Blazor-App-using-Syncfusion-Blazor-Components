76838620b335c261cbd3606f872390f03f1a8d70
Joshna Lingala Uthama Reddy Lingala <joshnalingala.u@syncfusion.com>
2022-01-12 14:45:02 +0530

    Adding Maui Blazor Sample

---
+++ MauiApp1/MauiApp1/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using MauiApp1.Data;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Syncfusion.Blazor;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .RegisterBlazorMauiWebView()
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddBlazorWebView();
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddSyncfusionBlazor();
+
+            return builder.Build();
+        }
+    }
+}
