2a6d8125ee4c43342d86b1e7897dc774be0d1bfe
Joseph Herson Thomas <josephherson.t@syncfusion.com>
2023-02-15 09:36:41 +0530

    Update the DataGrid example project

--- DataGrid Example/MauiApp1/App.xaml.cs
+++ DataGrid Example/MauiApp1/App.xaml.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
-using Application = Microsoft.Maui.Controls.Application;
+﻿namespace MauiApp1;
 
-namespace MauiApp1
+public partial class App : Application
 {
-    public partial class App : Application
-    {
-        public App()
-        {
-            InitializeComponent();
+	public App()
+	{
+		InitializeComponent();
 
-            MainPage = new MainPage();
-        }
-    }
+		MainPage = new MainPage();
+	}
 }
--- DataGrid Example/MauiApp1/Data/WeatherForecastService.cs
+++ DataGrid Example/MauiApp1/Data/WeatherForecastService.cs
@@ -1,25 +1,20 @@
-using System;
-using System.Linq;
-using System.Threading.Tasks;
+namespace MauiApp1.Data;
 
-namespace MauiApp1.Data
+public class WeatherForecastService
 {
-    public class WeatherForecastService
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+	private static readonly string[] Summaries = new[]
+	{
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	};
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-        {
-            var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
-        }
-    }
+	public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+	{
+		return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = startDate.AddDays(index),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+		}).ToArray());
+	}
 }
+
