f0569f8b9a78a92881b95e7d3b3f45a62222272a
Bharat Ram <bharatram.h@syncfusion.com>
2023-10-09 15:14:16 +0530

    feature(851665): Need to upgrade the GitHub samples of Maui Blazor app since it is outdated and we have been running into following compilation error.

--- Chart Example/MauiChartApp/App.xaml.cs
+++ Chart Example/MauiChartApp/App.xaml.cs
@@ -1,11 +1,12 @@
-﻿namespace MauiChartApp;
-
-public partial class App : Application
+﻿namespace MauiChartApp
 {
-	public App()
-	{
-		InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-		MainPage = new MainPage();
-	}
+            MainPage = new MainPage();
+        }
+    }
 }
